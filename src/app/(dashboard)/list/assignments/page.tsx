334c851493182a945044c07ae9ad5d0a01c5806d
Dost Mohammad Fahimi <dmfahimiiiii730@gmail.com>
2025-10-14 18:06:27 +0430

    fix the assigmnent page.

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -78,9 +78,7 @@ const AssignmentListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="felter" width={14} height={14} />
             </button>
-            {role === "admin" && (
-              <FormModal table="assignment" type="create"/>
-            )}
+            {role === "admin" && <FormModal table="assignment" type="create" />}
           </div>
         </div>
       </div>
